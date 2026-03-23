20a065ea5915109a062f05e847efd9a7f6f1e60e
kalishhhh <kaliaishit@gmail.com>
2026-03-23 14:01:13 +0530

    Use auth_username for PJSIP identity

--- asterisk/rootfs/app/server.py
+++ asterisk/rootfs/app/server.py
@@ -33,7 +33,7 @@ def generate_pjsip(endpoints):
 [global]
 type=global
 user_agent=HA-Asterisk
-endpoint_identifier_order=username,ip
+endpoint_identifier_order=auth_username,username,ip
 
 [transport-ws]
 type=transport
@@ -83,7 +83,7 @@ dtls_setup=actpass
 auth={user}-auth
 aors={user}-aor
 transport=transport-ws
-identify_by=username
+identify_by=auth_username
 
 [{user}-auth]
 type=auth
